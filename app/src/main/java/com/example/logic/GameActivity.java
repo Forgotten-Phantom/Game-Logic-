cc1aafaa673cfecb7bf5b9e49156f0ae92f597b6
MgChaous <darkmage2333@gmail.com>
2022-04-12 17:51:35 +0700

    made 3 buttons with logic in GameActivity, made 2 lines for Views in GameActivity

--- app/src/main/java/com/example/logic/GameActivity.java
+++ app/src/main/java/com/example/logic/GameActivity.java
@@ -1,10 +1,14 @@
 package com.example.logic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.google.android.material.card.MaterialCardView;
 
@@ -14,13 +18,15 @@ import java.util.concurrent.ThreadLocalRandom;
 public class GameActivity extends AppCompatActivity {
 
     MaterialCardView red;
-    Button green;
-    Button orange;
+    MaterialCardView green;
+    MaterialCardView orange;
     Button skyBlue;
     Button blue;
     Button yellow;
     Button pink;
     Button purple;
+    LinearLayout firstLine;
+    LinearLayout secondLine;
 
     int[] code = new int[5];
     int[] answer = new int[5];
@@ -39,6 +45,8 @@ public class GameActivity extends AppCompatActivity {
         yellow = findViewById(R.id.yellow);
         pink = findViewById(R.id.pink);
         purple = findViewById(R.id.purple);
+        firstLine = findViewById(R.id.firstLine);
+        secondLine = findViewById(R.id.secondLine);
 
         for (int i = 0; i < 5; i++)
             code[i] = ThreadLocalRandom.current().nextInt(1, 9);
@@ -46,8 +54,59 @@ public class GameActivity extends AppCompatActivity {
         red.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                answer[count] = 1;
+                count++;
 
+                CardView cardView = new CardView(getBaseContext());
+                cardView.setCardBackgroundColor(Color.RED);
+                cardView.setRadius(50);
+                cardView.setLayoutParams(new LinearLayout.LayoutParams(100, 100));
+                secondLine.addView(cardView);
+
+                if (count > 4) {
+                    count = 0;
+                    secondLine.removeAllViews();
+                }
             }
         });
+
+        green.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answer[count] = 2;
+                count++;
+
+                CardView cardView = new CardView(getBaseContext());
+                cardView.setCardBackgroundColor(Color.GREEN);
+                cardView.setRadius(50);
+                cardView.setLayoutParams(new LinearLayout.LayoutParams(100, 100));
+                secondLine.addView(cardView);
+
+                if (count > 4) {
+                    count = 0;
+                    secondLine.removeAllViews();
+                }
+            }
+        });
+
+        orange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answer[count] = 3;
+                count++;
+
+                CardView cardView = new CardView(getBaseContext());
+                cardView.setCardBackgroundColor(getBaseContext().getColor(R.color.orange));
+                cardView.setRadius(50);
+                cardView.setLayoutParams(new LinearLayout.LayoutParams(100, 100));
+                secondLine.addView(cardView);
+
+                if (count > 4) {
+                    count = 0;
+                    secondLine.removeAllViews();
+                }
+            }
+        });
+
     }
 }
